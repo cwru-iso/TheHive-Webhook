785dfa061f97ff7a2d6d68e2a441e831d9be8247
Kyle Colantonio <kyle.colantonio@case.edu>
2020-08-04 23:12:25 -0400

    Updated build script

--- build-layers.sh
+++ build-layers.sh
@@ -3,15 +3,18 @@ PYTHON_VERSION=3.7
 BUILD_TAG=thehive-lambda-layers
 
 # Rebuild image to make sure latest packages are downloaded
-cd docker
+echo "======== Pulling base docker image ========"
 docker pull "lambci/lambda:build-python$PYTHON_VERSION"
+
+echo "======== Building docker image ========"
+cd docker
 docker build . -t "$BUILD_TAG" --build-arg PYTHON_VERSION --no-cache
 cd ..
 
 # Run one-off command to copy new Zips to host
-echo "******** Copying completed layer builds to host ********"
-docker run --rm -it -v "$PWD":/app "$BUILD_TAG" bash -c ' yes | cp -rf /var/task/*.zip /app'
+echo "======== Copying completed layer builds to host ========"
+docker run --rm -it -v "$PWD":/app "$BUILD_TAG" bash -c ' yes | cp -rvf /var/task/*.zip /app'
 
 # Cleanup dangling images
-echo "******** Cleaning up dangling docker images ********"
+echo "======== Cleaning up dangling docker images ========"
 docker rmi $(docker images -f "dangling=true" -q) 2>/dev/null
